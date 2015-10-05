3667d50ffd650ad6e126ba39372dca538aef6ad6
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-10-05 16:55:09 +0200

    Added SECURE and LOGIN_CAP options for svdinit build.

--- src/svdsysup.c
+++ src/svdsysup.c
@@ -2,6 +2,9 @@
  * Copyright (c) 1991, 1993
  *  The Regents of the University of California.  All rights reserved.
  *
+ * Copyright (c) 2015
+ *  Daniel (dmilith) Dettlaff. All rights reserved.
+ *
  * This code is derived from software contributed to Berkeley by
  * Donn Seeley at Berkeley Software Design, Inc.
  *
